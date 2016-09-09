2af12ed3f350ee42fada45c11c96422ac7a9094f
Roberto Arroyo <rob.arroyo.m@gmail.com>
2016-09-09 15:41:12 -0700

    Initial Commit

---
+++ testGIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testGIt
+//
+//  Created by Roberto Arroyo on 9/9/16.
+//  Copyright © 2016 Roberto Arroyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
